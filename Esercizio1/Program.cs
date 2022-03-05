535bb35c2ea113db9702f45c377ed39e162c8296
gupta-mukund <97413360+gupta-mukund@users.noreply.github.com>
2022-03-05 11:31:48 +0100

    First

---
+++ Esercizio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Esercizio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCallback(new int[] { 2, 4, 5, 6, 0, 0, -2, -8 }, CheckNumbers);
+        }
+        static void RunCallback(int[] numbers, Action<int[]> functionDel)
+        {
+            functionDel(numbers);
+        }
+        static void CheckNumbers(int[] numbers)
+        {
+            foreach (int n in numbers)
+            {
+                if (n < 0) Console.WriteLine($"{n} < di zero");
+                else if (n > 0) Console.WriteLine($"{n} > di zero");
+                else Console.WriteLine($"{n} uguale a zero");
+            }
+        }
+    }
+}
---
+++ Esercizio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Esercizio2
+{
+    class Program
+    {
+        public delegate void FunctionDel(int n);
+        static void Main(string[] args)
+        {
+            FunctionDel functions = new FunctionDel(NumberSquare);
+            functions += NumberCube;
+            functions.Invoke(2);
+        }
+
+        static public void NumberSquare(int n)
+        {
+            Console.WriteLine($"The square of {n} is {Math.Pow(n, 2)}");
+        }
+
+        static public void NumberCube(int n)
+        {
+            Console.WriteLine($"The cube of {n} is {Math.Pow(n, 3)}");
+        }
+    }
+}

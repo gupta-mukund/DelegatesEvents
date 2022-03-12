48a699eaceb1ca45d24f67e722c2a9f4894550ce
Mukund Gupta <6190006@itisrossi.vi.it>
2022-03-12 14:38:57 +0100

    Update

--- linq/Esercizio/Form1.cs
+++ linq/Esercizio/Form1.cs
@@ -18,6 +18,20 @@ namespace Esercizio
             InitializeComponent();
             data = new string[] { "Antonella Turso", "Alberto Costa", "Donatella Bertoletti", "Paolo Brunelli", "Fabrizio Lovison", "Paolo De Rigo", "Mattia Marziale", "Salvatore Spinella", "Chiara Perrotta", "Giorgio Tagliapietra", "Raffaella Borrelli" };
             lsbFull.DataSource = data;
+
+            string[] data1 = (from d in data
+                        where d.StartsWith("A") || d.StartsWith("C")
+                        orderby "A"                        
+                        select d).ToArray();
+            string[] data2 = (from d in data
+                             where d.Length < 14
+                             select d).ToArray();
+            string[] data3 = (from d in data
+                             where d.Length < 16 && !d.StartsWith("A")
+                             select d).ToArray();
+            lsbAC.DataSource = data1;
+            lsb14Char.DataSource = data2;
+            lsb16Char.DataSource = data3;
         }
     }
 }
